0fdfe9e730fd85c9c167fe5e8cf864a5b2b20c0a
Merin Anna Biju <merinannabiju2024@gmail.com>
2026-07-17 15:19:59 +0530

    Completed payment module

--- server/server.js
+++ server/server.js
@@ -1,5 +1,11 @@
 const express = require("express");
 const cors = require("cors");
+const dotenv = require("dotenv");
+const connectDB = require("./config/db");
+const paymentRoutes = require("./routes/paymentRoutes");
+
+dotenv.config();
+connectDB();
 
 const app = express();
 
@@ -10,7 +16,9 @@ app.get("/", (req, res) => {
     res.send("Cloud9 Backend Running");
 });
 
-const PORT = 5000;
+app.use("/api/payment", paymentRoutes);
+
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
