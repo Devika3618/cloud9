8aa4dc19a809ae4a7a34fc505eea05c4882c96a6
mehjabin-akbar <mehjabinakbar@gmail.com>
2026-07-17 23:02:08 +0530

    Added backend authentication module

--- server/server.js
+++ server/server.js
@@ -1,13 +1,27 @@
 const express = require("express");
 const cors = require("cors");
+const connectDB = require("./config/db");
+const userRoutes = require("./routes/userRoutes");
+const protect = require("./middleware/authMiddleware");
+
+connectDB();
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
+app.use("/api/users", userRoutes);
+
+app.get("/api/profile", protect, (req, res) => {
+    res.json({
+        message: "Welcome! This is a protected route.",
+        user: req.user
+    });
+});
+
 app.get("/", (req, res) => {
-    res.send("Cloud9 Backend Running");
+    res.send("Cloud9 Ecommerce API Running");
 });
 
 const PORT = 5000;
