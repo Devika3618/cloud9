09627b7b7d2160f6819941aeb4ab7ca64e6cbc4c
Devapriya Prejith <devapriyadevu098@gmail.com>
2026-07-18 12:44:41 +0530

    done

--- client/src/App.js
+++ client/src/App.js
@@ -11,6 +11,13 @@ import Cart from "./pages/Cart";
 import Checkout from "./pages/Checkout";
 import Profile from "./pages/Profile";
 import AdminDashboard from "./pages/AdminDashboard";
+import Dashboard from "./admin/pages/Dashboard";
+import Products from "./admin/pages/ManageProducts";
+import Orders from "./admin/pages/ManageOrders";
+import Users from "./admin/pages/ManageUsers";
+
+
+  
 
 import MainLayout from "./layouts/MainLayout";
 
@@ -29,6 +36,11 @@ const App = () => {
         <Route path="/checkout" element={<Checkout />} />
         <Route path="/profile" element={<Profile />} />
         <Route path="/admin" element={<AdminDashboard />} />
+        <Route path="/admin" element={<Dashboard />} />
+        <Route path="/admin/products" element={<Products />} />
+        <Route path="/admin/orders" element={<Orders />} />
+        <Route path="/admin/users" element={<Users />} />
+
       </Routes>
     </MainLayout>
   );
--- client/src/pages/AdminDashboard.jsx
+++ client/src/pages/AdminDashboard.jsx
@@ -1,19 +1,29 @@
+import { useNavigate } from "react-router-dom";
+
 const AdminDashboard = () => {
+  const navigate = useNavigate();
+
   return (
-    <div style={{ padding: "40px", textAlign: "center" }}>
+    <div>
       <h1>Admin Dashboard</h1>
 
-      <button>Manage Products</button>
+      <button onClick={() => navigate("/admin/products")}>
+        Manage Products
+      </button>
 
       <br />
       <br />
 
-      <button>Manage Orders</button>
+      <button onClick={() => navigate("/admin/orders")}>
+        Manage Orders
+      </button>
 
       <br />
       <br />
 
-      <button>Manage Users</button>
+      <button onClick={() => navigate("/admin/users")}>
+        Manage Users
+      </button>
     </div>
   );
 };
